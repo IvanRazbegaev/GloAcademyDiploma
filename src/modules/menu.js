64e33ef87636e88cbdf7066a799d40d9beecc2d1
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-18 01:17:05 +0300

    added also close button handling

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,13 +1,22 @@
 const menu = () => {
   const menuIcon = document.querySelector('.menu__icon');
-  const popup = document.querySelector('.popup-dialog-menu')
+  const popup = document.querySelector('.popup-dialog-menu');
+  const closeBtn = document.querySelector('.close-menu');
 
   menuIcon.addEventListener('click', () => {
 
-    if(document.documentElement.offsetWidth === 576){
+    if(document.documentElement.offsetWidth <= 576){
       popup.style.transform = 'translateY(0%)';
     }else {
       popup.style.transform = 'translateX(0%)'
+    }
+  })
+
+  closeBtn.addEventListener('click', () => {
+    if(document.documentElement.offsetWidth <= 576){
+      popup.style.transform = 'translateY(-100%)';
+    }else {
+      popup.style.transform = 'translateX(100%)'
     }
   })
 }
