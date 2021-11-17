1d73018188f539deb756b0e72e20445724edb2dd
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-18 01:10:16 +0300

    saving code

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,13 +1,23 @@
 const menu = () => {
   const menuIcon = document.querySelector('.menu__icon');
   const popup = document.querySelector('.popup-dialog-menu')
+  const closeBtn = document.querySelector('.close-menu');
 
-  menuIcon.addEventListener('click', () => {
-
-    if(document.documentElement.offsetWidth === 576){
-      popup.style.transform = 'translateY(0%)';
+  menuIcon.addEventListener('click', (e) => {
+    if(document.documentElement.offsetWidth <= 576){
+      popup.style.transform = 'translate3d(0, -100%, 0)';
+      popup.style.transform = 'translate3d(0, 0%, 0)';
     }else {
-      popup.style.transform = 'translateX(0%)'
+      popup.style.transform = 'translate3d(100%, 0, 0)'
+      popup.style.transform = 'translate3d(0%, 0, 0)'
+    }
+  })
+
+  closeBtn.addEventListener('click', () => {
+    if(document.documentElement.offsetWidth <= 576){
+      popup.style.transform = 'translate3d(0,-100%, 0)';
+    }else {
+      popup.style.transform = 'translate3d(100%, 0, 0)'
     }
   })
 }
