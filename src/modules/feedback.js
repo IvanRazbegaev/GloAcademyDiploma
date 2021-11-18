1114755b057f87548d50685ad4d8ce218560d9a4
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-18 20:38:20 +0300

    all done exept for fetch part

--- src/modules/feedback.js
+++ src/modules/feedback.js
@@ -4,16 +4,26 @@ const feedback = () => {
 
   feedbackForm.forEach(item => {
     const phoneInput = item.querySelector('.input.feedback__input-input');
-    const feedbackBtn = item.querySelector('.button')
+    const feedbackBtn = item.querySelector('.button');
+    let confCheckbox = item.querySelector('.checkbox__input');
+    console.dir(confCheckbox);
+
     phoneInput.addEventListener('input', (e) => {
       const pattern = /[^\d+\-\)\(]/gi
       e.target.value = e.target.value.replace(pattern, '')
     })
     feedbackBtn.addEventListener('click', async (e) => {
+      confCheckbox = item.querySelector('.checkbox__input');
       e.preventDefault();
       if (phoneInput.value.length < 11){
         alert('Телефонный номер не может быть менее 11 символов!')
         return false;
+      } else if (!confCheckbox.checked) {
+        alert('Примите политику безопасности!')
+        return false;
+      } else {
+        phoneInput.value = '';
+        confCheckbox.checked = false;
       }
       const response = await sendFeedback(phoneInput.value);
       console.log(response);
