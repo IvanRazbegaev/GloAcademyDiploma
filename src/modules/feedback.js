a5fe795bfede3543e04739eb6433f8aa2b26683e
Ivan Razbegaev <sinnless13+1@gmail.com>
2021-11-18 12:04:25 +0300

    added function for sending data

--- src/modules/feedback.js
+++ src/modules/feedback.js
@@ -4,12 +4,26 @@ const feedback = () => {
 
 feedbackForm.forEach(item => {
   const phoneInput = item.querySelector('.input.feedback__input-input');
+  const feedbackBtn = item.querySelector('.button')
   phoneInput.addEventListener('input', (e) => {
     const pattern = /[^\d+\-\)\(]/gi
     e.target.value = e.target.value.replace(pattern, '')
   })
-
+  feedbackBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const response = await sendFeedback(phoneInput.value);
+    console.log(response);
   })
+  })
+}
+
+const sendFeedback = async (data) => {
+  const path = 'server.php'
+  return await fetch(path,{
+    method: 'POST',
+    body: JSON.stringify(data)
+  }).then(response => response.json())
+    .catch((e) => console.log(e));
 }
 
 export default feedback;
