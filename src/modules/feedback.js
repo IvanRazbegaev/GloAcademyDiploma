95c2d143f9e80e48010748cf04a0b4ef053d5e7b
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-18 19:52:51 +0300

    saving code

--- src/modules/feedback.js
+++ src/modules/feedback.js
@@ -4,11 +4,36 @@ const feedback = () => {
 
   feedbackForm.forEach(item => {
     const phoneInput = item.querySelector('.input.feedback__input-input');
+    const feedbackBtn = item.querySelector('.button')
     phoneInput.addEventListener('input', (e) => {
       const pattern = /[^\d+\-\)\(]/gi
       e.target.value = e.target.value.replace(pattern, '')
     })
+    feedbackBtn.addEventListener('click', async (e) => {
+      e.preventDefault();
+      if (phoneInput.value.length < 11){
+        alert('Телефонный номер не может быть менее 11 символов!')
+        return false;
+      }
+      const response = await sendFeedback(phoneInput.value);
+      console.log(response);
+    })
   })
+}
+
+const sendFeedback = async (data) => {
+  const path = 'server.php'
+  const getData = await fetch(path,{
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }).then(response => response.json())
+    .catch((e) => console.log(e));
+
+  return getData;
 }
 
 export default feedback;
