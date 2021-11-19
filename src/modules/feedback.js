b0604616b6eec0e33f42b00c099b8a743c86ed89
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-19 21:18:07 +0300

    partly done

--- src/modules/feedback.js
+++ src/modules/feedback.js
@@ -8,10 +8,16 @@ const feedback = () => {
     let confCheckbox = item.querySelector('.checkbox__input');
     console.dir(confCheckbox);
 
-    phoneInput.addEventListener('input', (e) => {
-      const pattern = /[^\d+\-\)\(]/gi
-      e.target.value = e.target.value.replace(pattern, '')
+    const feedbackForm = document.querySelectorAll('.feedback');
+
+    feedbackForm.forEach(item => {
+      const phoneInput = item.querySelector('.input.feedback__input-input');
+      phoneInput.addEventListener('input', (e) => {
+        const pattern = /[^\d+\-\)\(]/gi
+        e.target.value = e.target.value.replace(pattern, '')
+      })
     })
+
     feedbackBtn.addEventListener('click', async (e) => {
       confCheckbox = item.querySelector('.checkbox__input');
       e.preventDefault();
@@ -22,23 +28,22 @@ const feedback = () => {
         alert('Примите политику безопасности!')
         return false;
       } else {
-        phoneInput.value = '';
         confCheckbox.checked = false;
       }
       const response = await sendFeedback(phoneInput.value);
+      phoneInput.value = '';
       console.log(response);
     })
   })
 }
 
 const sendFeedback = async (data) => {
-  const path = 'server.php'
+  const path = 'https://en22hlwqnbzi7pj.m.pipedream.net'
   const getData = await fetch(path,{
     method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      'Content-type': 'application/json',
-      'Accept': 'application/json'
+      'Content-type': 'application/json'
     }
   }).then(response => response.json())
     .catch((e) => console.log(e));
