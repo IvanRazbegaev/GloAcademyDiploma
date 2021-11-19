a2c14a0bee59f4043317bd39918479c31309e711
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-19 18:58:42 +0300

    partly done

--- src/modules/popupAnnotation.js
+++ src/modules/popupAnnotation.js
@@ -1,18 +1,18 @@
 const popupAnnotation = () => {
   const formulaForm = document.querySelector('.wrapper_small');
   const formulaItemsPopup = formulaForm.querySelectorAll('.formula-item-popup');
+  const formulaItems = formulaForm.querySelectorAll('.formula-item');
 
-  formulaForm.addEventListener('mouseover', (e) => {
-    if (e.target.closest(".formula-item")){
-      console.dir(e.target)
-      // formulaItemsPopup.style.opacity = '1';
-      // formulaItemsPopup.style.visibility = 'visible';
-      console.log("Мы на элементе!")
-    } else {
-      // formulaItemsPopup.style.opacity = '0.1';
-      // formulaItemsPopup.style.visibility = 'hidden';
-      console.log("Мы не на элементе!")
-    }
+  formulaItems.forEach((item, index) => {
+    item.addEventListener('mouseover', (e) => {
+      if (!e.target.classList.contains("formula-item")){
+        formulaItemsPopup[index].style.opacity = '1';
+        formulaItemsPopup[index].style.visibility = 'visible';
+      } else {
+        formulaItemsPopup[index].style.opacity = '0.2';
+        formulaItemsPopup[index].style.visibility = 'hidden';
+      }
+    })
   })
 }
 export default popupAnnotation;
