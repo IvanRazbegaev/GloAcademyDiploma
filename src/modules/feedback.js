098e68453d22953063f7e615c0fcd39bc6bf2635
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-19 21:21:19 +0300

    the hell is up with main.js?!

--- src/modules/feedback.js
+++ src/modules/feedback.js
@@ -10,7 +10,7 @@ const feedback = () => {
   })
   function phoneMask (phone) {
     return phone.replace(/\D/g, '')
-      .replace(/^(\d{1,2})(\d{3})(\d{3})(\d{2})(\d{2})/, '+$1($2)$3-$4-$5')
+      .replace(/^(\d{1,2})(\d{3})(\d{3})(\d{2})(\d{2})/, '+$1($2)$3-$4-$5');
   }
 }
 
