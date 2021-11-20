9f0075f124565fa8953b213dd8a8565f716afd9c
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-20 10:33:13 +0300

    some fixes in smoothscroll

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,14 +1,5 @@
 const menu = () => {
   const menuIcon = document.querySelector('.menu__icon');
-<<<<<<< HEAD
-  const popup = document.querySelector('.popup-dialog-menu');
-  const closeBtn = document.querySelector('.close-menu');
-
-  menuIcon.addEventListener('click', () => {
-
-    if(document.documentElement.offsetWidth <= 576){
-      popup.style.transform = 'translateY(0%)';
-=======
   const popup = document.querySelector('.popup-dialog-menu')
   const closeBtn = document.querySelector('.close-menu');
   const menuItem = document.querySelectorAll('.popup-menu-nav__item > a');
@@ -17,29 +8,19 @@ const menu = () => {
   const menuClosing = () => {
     if(document.documentElement.offsetWidth <= 576){
       popup.style.transform = 'translate3d(0,-100%, 0)';
->>>>>>> smoothscroll
     }else {
       popup.style.transform = 'translate3d(100%, 0, 0)'
     }
   }
 
   menuIcon.addEventListener('click', () => {
-      popup.style.transform = 'translate3d(0, 0%, 0)';
+    popup.style.transform = 'translate3d(0, 0%, 0)';
   })
 
   closeBtn.addEventListener('click', () => {
     menuClosing();
   })
 
-<<<<<<< HEAD
-  closeBtn.addEventListener('click', () => {
-    if(document.documentElement.offsetWidth <= 576){
-      popup.style.transform = 'translateY(-100%)';
-    }else {
-      popup.style.transform = 'translateX(100%)'
-    }
-  })
-=======
   menuItem.forEach(item => {
     item.addEventListener('click', (e) => {
       const href = document.querySelector(item.getAttribute("href"));
@@ -55,7 +36,6 @@ const menu = () => {
     href.scrollIntoView({block:"center", behavior: "smooth"})
   })
 
->>>>>>> smoothscroll
 }
 
 export default menu;
