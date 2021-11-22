08ce5ab9ef75320cba261d480dff4cc5bbaa0123
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-23 00:39:05 +0300

    partly done

--- src/modules/contracts.js
+++ src/modules/contracts.js
@@ -1,25 +1,78 @@
 const contracts = () => {
-  const contractsForm = document.querySelector('.transparency-slider');
-  const contract = document.querySelector('.popup-transparency');
+  const contractsList = document.querySelectorAll('.transparency-item');
+  const contractForm = document.querySelector('.popup.popup-transparency');
+  const contract = document.querySelectorAll('.popup-transparency-slider__slide');
 
-  console.log(contract);
+  const currentCount = contractForm.querySelector('.slider-counter-content__current');
+  const overallCount = contractForm.querySelector('.slider-counter-content__total');
 
-  contractsForm.addEventListener('click', (e) => {
-    if(e.target.matches('.transparency-item__img')){
-      contract.style.visibility = 'visible';
-    }
-  })
+  console.dir(currentCount)
+  console.dir(overallCount)
 
-  contract.addEventListener('click', (e) => {
+  let currentContract = 0;
+  let slidesOverall = contract[currentContract].querySelectorAll('img');
+  let currentSlide = 1;
 
-    if (!e.target.closest('.popup-dialog-transparency')) {
-      if (e.target.classList.contains('popup-arrow_transparency_right') || e.target.classList.contains('popup-arrow_transparency_left')) {
-        console.log('arrows were clicked')
+
+
+  for (let i = 0; i < contractsList.length; i++){
+    contractsList[i].addEventListener('click', (e) => {
+      hideContracts();
+      contractForm.style.visibility = 'visible';
+      contract[i].style.display = 'block';
+      currentContract = i;
+      updateCounter(i);
+    })
+  }
+
+    contractForm.addEventListener('click', (e) => {
+      if (e.target.closest('.popup-dialog-transparency')) {
+        if(e.target.closest('.popup-arrow_transparency_right') || e.target.closest('.popup-arrow_transparency_left')){
+          if (e.target.closest('.popup-arrow_transparency_right')){
+            slidesOverall = contract[currentContract].querySelectorAll('img');
+            console.log(currentSlide);
+            prevSlide(slidesOverall, currentSlide)
+            currentSlide++;
+            nextSlide(slidesOverall, currentSlide)
+            if (currentSlide > slidesOverall.length){
+              currentSlide = 1;
+            }
+            updateCounter();
+          } else if (e.target.closest('.popup-arrow_transparency_left')) {
+            slidesOverall = contract[currentContract].querySelectorAll('img');
+            console.log(currentSlide);
+            prevSlide(slidesOverall, currentSlide)
+            currentSlide--;
+            nextSlide(slidesOverall, currentSlide)
+            if (currentSlide <= 0){
+              currentSlide = slidesOverall.length;
+            }
+            updateCounter();
+          }
+        }
       } else {
-        contract.style.visibility = 'hidden';
+        contractForm.style.visibility = 'hidden';
       }
+    })
+  const prevSlide = (elem, index) => {
+    elem[index - 1].style.display = 'none';
+  }
+
+  const nextSlide = (elem, index) => {
+    elem[index - 1].style.display = 'block';
+  }
+
+  const hideContracts = () => {
+    for (let i = 0; i < contract.length; i++){
+      contract[i].style.display = 'none'
     }
-  })
+  }
+
+  const updateCounter = (currentContract) => {
+    currentCount.textContent = `${currentSlide}`;
+    slidesOverall = contract[currentContract].querySelectorAll('img');
+    overallCount.textContent = `${slidesOverall.length}`;
+  }
 }
 
 export default contracts;
