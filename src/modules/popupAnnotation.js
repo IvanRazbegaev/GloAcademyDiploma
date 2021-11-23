0c4cef3a636e3da0b90e3848fc6e21fc69f3d26e
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-23 23:33:31 +0300

    done active menu for desktop and mobile

--- src/modules/popupAnnotation.js
+++ src/modules/popupAnnotation.js
@@ -1,44 +1,63 @@
 const popupAnnotation = () => {
   const formulaForm = document.querySelector('.formula');
-  const formulaItemsPopup = formulaForm.querySelectorAll('.formula-item-popup');
-  let formulaItems = document.querySelectorAll('.formula-item');
-
-  console.log(formulaItems)
-
+  const formulaItemsMobile = formulaForm.querySelectorAll('.formula-item.formula-slider__slide');
+  const navArrowLeft = document.getElementById('formula-arrow_left');
+  const navArrowRight = document.getElementById('formula-arrow_right');
   const mobileVersionWidth = 1024;
 
-  let activeElemPosition = 0;
+  let formulaItemsDesktop = document.querySelectorAll('.wrapper_small.mobile-hide.tablet-hide>.row>.formula-item');
   let activeItem = 0;
 
-  formulaForm.addEventListener('mouseover', (e) => {
-    console.log(e.target)
-    if(e.target.matches('.formula-item__icon')){
-      console.log('Над иконкой')
-    }
+  formulaItemsDesktop.forEach((item, index) => {
+    item.addEventListener('mouseover', (e) => {
+      if (e.target.closest(".formula-item__icon")){
+        item.classList.add('active-item');
+
+      } else {
+        item.classList.remove('active-item');
+      }
+    })
   })
-  // forEach((item, index) => {
-  //   item.addEventListener('mouseover', (e) => {
-  //     if (e.target.closest(".formula-item__icon")){
-  //       item.classList.add('active-item');
-  //
-  //     } else {
-  //       item.classList.remove('active-item');
-  //     }
-  //   })
-  // })
-  // const getActiveElemPosition = (elem, index) => {
-  //       activeElemPosition = elem[index].getBoundingClientRect().top + window.scrollY;
-  // }
-  //
-  // const getActiveItem = () => {
-  //   formulaItems = document.querySelectorAll('.formula-item');
-  //   for (let i = 0; i < formulaItems; i++){
-  //     console.log(formulaItems[i]);
-  //     if(formulaItems[i].classList.contains('active-item')){
-  //       activeItem = i;
-  //       console.log(activeItem);
-  //     }
-  //   }
-  // }
+
+  const showSlide= (elem, index) => {
+    elem[index].style.display = 'flex';
+    elem[index].classList.add('active-item');
+  }
+
+  const exposeOfSlide = (elem, index) => {
+    elem[index].style.display = 'none';
+    elem[index].classList.remove('active-item');
+  }
+
+  const hideSlides = () => {
+    for  (let i = 0; i < formulaItemsMobile.length; i++){
+      formulaItemsMobile[i].style.display = 'none'
+    }
+  }
+
+  if (document.documentElement.offsetWidth <= mobileVersionWidth){
+    hideSlides();
+    showSlide(formulaItemsMobile,activeItem);
+    navArrowLeft.addEventListener('click', () => {
+      exposeOfSlide(formulaItemsMobile, activeItem);
+      activeItem--;
+      console.log(activeItem);
+      if (activeItem < 0){
+        activeItem = formulaItemsMobile.length -1;
+      }
+      showSlide(formulaItemsMobile, activeItem)
+    })
+    navArrowRight.addEventListener('click', () => {
+      exposeOfSlide(formulaItemsMobile, activeItem);
+      console.log(activeItem);
+      activeItem++;
+      if (activeItem >= formulaItemsMobile.length){
+        activeItem = 0;
+      }
+      showSlide(formulaItemsMobile, activeItem)
+    })
+  }
+
+
 }
 export default popupAnnotation;
