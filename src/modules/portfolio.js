f96e1c6400dca2de4585565093ca53b0a998d825
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-23 23:53:45 +0300

    added handling of left button

--- src/modules/portfolio.js
+++ src/modules/portfolio.js
@@ -1,11 +1,16 @@
 const portfolio = () => {
   const portfolioForm = document.querySelector('.portfolio');
+  const portfolioSlide = portfolioForm.querySelectorAll('.portfolio-slider__slide.fade-tab');
+  const arrowLeft = document.getElementById('portfolio-arrow_right')
 
-  portfolioForm.addEventListener('click', (e) => {
-    if(e.target.matches('#portfolio-arrow_right')){
-      console.log('arrow right is clicked')
-    }
-  })
+  let currentMargin = 0;
+  console.log(portfolioSlide)
+
+  arrowLeft.addEventListener('click', () => {
+    console.log('arrow is clicked')
+    currentMargin += +portfolioSlide[0].offsetWidth;
+    portfolioSlide[0].style.marginLeft = `-${currentMargin}px`;
+    })
 }
 
 export default portfolio;
