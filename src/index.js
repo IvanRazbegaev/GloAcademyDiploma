84da5cf9e8ed57469c7edcf7b68afea0a98c34c0
Ivan Razbegaev <sinnless13@gmail.com>
2021-11-25 20:26:11 +0300

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import accordion from "./modules/accordeon";
+import consultation from "./modules/consultation";
+import contracts from "./modules/contracts";
+import feedback from "./modules/feedback";
+import headerPhones from "./modules/headerPhones";
+import {menu} from "./modules/menu";
+import modal from "./modules/modal";
+import portfolio from "./modules/portfolio";
+import repairSlider from "./modules/repairSlider";
 
 accordion();
+consultation();
+contracts();
+feedback('.feedback');
+headerPhones();
+menu();
+modal();
+portfolio();
+repairSlider();
