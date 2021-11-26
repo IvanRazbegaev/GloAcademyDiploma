02d64aa44ff7d5c53f6e6d1dd7d944bb277f0911
Ivan Razbegaev <sinnless13+1@gmail.com>
2021-11-26 11:35:54 +0300

    portfolio - added popup pictures, text and navigation handling

--- src/modules/portfolio.js
+++ src/modules/portfolio.js
@@ -5,19 +5,35 @@ const portfolio = () => {
   const arrowLeft = document.getElementById('portfolio-arrow_left');
   const slidesList = portfolioForm.querySelectorAll('.portfolio-slider__slide-frame');
   const portfolioPopup = document.querySelector('.popup-portfolio');
-  const popupDialogWindow = portfolioPopup.querySelector('.popup-dialog')
+  const sliderText = portfolioPopup.querySelectorAll('.popup-portfolio-text');
+  const SliderPicture = portfolioPopup.querySelectorAll('.popup-portfolio-slider__slide')
+  const contentCounterTotal = portfolioPopup.querySelector('.slider-counter-content__total');
+  const popupArrowLeft = portfolioPopup.querySelector('.popup-arrow.popup-arrow_left');
+  const popupArrowRight = portfolioPopup.querySelector('.popup-arrow.popup-arrow_right');
 
+  let contentCounterCurrent = portfolioPopup.querySelector('.slider-counter-content__current');
   let currentlyShownSlides;
   let activeSet = [];
+  let activePicture = 0;
+
+
+  contentCounterTotal.textContent = `${SliderPicture.length}`;
+
+  const hidePictures = (elem, indexes) => {
+    for (let i = 0; i < indexes; i++){
+      elem[i].style.display = 'none';
+    }
+  }
+  const showSlide = (elem, index) => {
+    elem[index].style.display = 'flex';
+  }
 
   if (document.documentElement.offsetWidth < 1010){
     currentlyShownSlides = 2;
-  } else {
+  } else if (document.documentElement.offsetWidth >= 1010) {
     currentlyShownSlides = 3;
-  }
-
-  const showSlide = (elem, index) => {
-    elem[index].style.display = 'flex';
+  } else if (document.documentElement.offsetWidth < 560) {
+    currentlyShownSlides = 1;
   }
 
   const hideSlide = (elem, index) => {
@@ -94,9 +110,15 @@ const portfolio = () => {
     }
   })
 
-  slidesList.forEach(slide => {
+  slidesList.forEach((slide, index) => {
     slide.addEventListener('click', (e) => {
       portfolioPopup.style.visibility = 'visible';
+      hidePictures(sliderText, sliderText.length);
+      hidePictures(SliderPicture, sliderText.length);
+      showSlide(sliderText, index.toString().slice(-1));
+      showSlide(SliderPicture, index.toString().slice(-1));
+      activePicture = +index.toString().slice(-1);
+      contentCounterCurrent.textContent = `${activePicture + 1}`;
     })
   })
 
@@ -110,10 +132,30 @@ const portfolio = () => {
     }
   })
 
-  const slideInit = () => {
-    const sliderText = portfolioPopup;
-    //TODO сделать функцию показа элементов попапа, запускать 1 раз (?)
-  }
+  popupArrowLeft.addEventListener('click', () => {
+    activePicture--
+    if (activePicture < 0){
+      activePicture = SliderPicture.length - 1;
+    }
+    hidePictures(sliderText, sliderText.length);
+    hidePictures(SliderPicture, sliderText.length);
+    showSlide(sliderText, activePicture);
+    showSlide(SliderPicture, activePicture);
+    contentCounterCurrent.textContent = `${activePicture + 1}`;
+  })
+
+  popupArrowRight.addEventListener('click', () => {
+    activePicture++
+    if (activePicture >= SliderPicture.length){
+      activePicture = 0;
+    }
+    hidePictures(sliderText, sliderText.length);
+    hidePictures(SliderPicture, sliderText.length);
+    showSlide(sliderText, activePicture);
+    showSlide(SliderPicture, activePicture);
+    contentCounterCurrent.textContent = `${activePicture + 1}`;
+  })
+
 }
 
 export default portfolio;
